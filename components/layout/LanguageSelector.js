e150b32cf8f7ecc44ac23b37198e2bcda3e9f1c4
Cesar Redondo <credondocr@gmail.com>
2020-05-31 13:56:17 -0600

    Fix language switcher styles

--- components/layout/LanguageSelector.js
+++ components/layout/LanguageSelector.js
@@ -1,15 +1,39 @@
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { i18n, withTranslation } from '../../i18n';
 
+const Button = styled.button`
+  border-radius: 50%;
+  color: white;
+  background-color: orange;
+  border: none;
+`;
+
+const Span = styled.span`
+  padding: 2px;
+  color: white;
+`;
+
+const Container = styled.div`
+  margin: 1em;
+`;
+
 const LanguageSelector = ({ t }) => (
-  <div className="laguage-selector">
-    <button type="button" onClick={() => i18n.changeLanguage('en')}>
-      {t('language.english')}
-    </button>
-    <button type="button" onClick={() => i18n.changeLanguage('es')}>
-      {t('language.spanish')}
-    </button>
-  </div>
+  <Container>
+    <Span>
+      {t('lanLabel')}
+    </Span>
+    <Span>
+      <Button type="button" onClick={() => i18n.changeLanguage('en')}>
+        {t('language.english')}
+      </Button>
+    </Span>
+    <Span>
+      <Button type="button" onClick={() => i18n.changeLanguage('es')}>
+        {t('language.spanish')}
+      </Button>
+    </Span>
+  </Container>
 );
 
 LanguageSelector.propTypes = {
