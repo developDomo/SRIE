a86c36ff43db62203984227bc93eaf59b09fd022
Cesar Redondo <credondocr@gmail.com>
2020-06-01 09:38:18 -0600

    pr change

--- components/layout/NavBar.js
+++ components/layout/NavBar.js
@@ -70,16 +70,13 @@ export default class NavBar extends React.Component {
         </Row>
         <style type="text/css">
           {`
-            
             .nav-link{
                 font-family: 'Raleway', sans-serif;
                 text-transform: uppercase;
                 font-weight: bold;
-                color: white !important;
             }
             .CName .nav-link{
                 color: white !important;
-                
             }
             a.nav-item-drop {
               color: white !important;
@@ -116,7 +113,7 @@ export default class NavBar extends React.Component {
                  width: 0;
             }
             .navbar-light .navbar-toggler {
-                color: rgba(0,0,0,.5);
+                color: white;
                 border: none;
                 margin: 9px auto 0 auto;
             }
@@ -124,7 +121,9 @@ export default class NavBar extends React.Component {
               /*color: white;*/
               font-size: 1.5em;
           }
-          
+          .navbar-light .navbar-nav .nav-link {
+            color: white;
+          }
 
         `}
         </style>
