7e7c38656d8ca6874169b2a9f7e03fefe28ae2e3
lfitoria <lfitoria@eldomo.net>
2020-07-06 09:30:52 -0600

    add Indicators Translation in En

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -35,9 +35,7 @@ const Header = ({ router: { pathname } }) => {
     if (item2.isCountry) {
       return (
         <Link href="/[id]" as={`/${item2.href}`} key={item2.href}>
-          {' '}
           <a className={`${path === `/${item2.href}` ? 'nav-item-drop active-link' : 'nav-item-drop'}`}>{item2.label}</a>
-          {' '}
         </Link>
       );
     }
