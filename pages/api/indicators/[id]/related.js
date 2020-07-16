240f275d93637f2c206f49a89dd05afd578fc730
Javier Camacho <camacho.fj@gmail.com>
2020-07-15 19:51:46 -0600

    CU-94hg85 Add indicators 22, 23 and 26 plus update all endpoint (#58)

--- pages/api/indicators/[id]/related.js
+++ pages/api/indicators/[id]/related.js
@@ -1,11 +1,24 @@
 import nextConnect from 'next-connect';
 import IndicatorService from '../../../../services/Indicator.service';
+import InputValidatorUtils from '../../../../utils/InputValidator.utils';
 
 const handler = nextConnect();
 
+const schema = {
+  id: {
+    type: 'number', positive: true, integer: true, convert: true,
+  },
+};
+
+const inputValidator = InputValidatorUtils.getInputValidator(schema);
+
 handler.get(async (req, res) => {
-  const relatedIndicators = await IndicatorService.findRelated(req.query.id);
-  res.status(200).json(relatedIndicators);
+  if (InputValidatorUtils.validate(inputValidator, req.query, res)) {
+    const relatedIndicators = await IndicatorService.findRelated(req.query.id);
+    res.status(200).json(relatedIndicators);
+  } else {
+    res.status(400).json({ error: true, message: 'Bad request' });
+  }
 });
 
 export default (req, res) => handler.apply(req, res);
