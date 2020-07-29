2295c1eba21ee5b4e7f1cc0e7e52decd70f25a85
Francisco Campos <hey@francampos.me>
2020-07-28 18:44:30 -0600

    traslate welcometext

--- components/layout/WelcomeText.js
+++ components/layout/WelcomeText.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import { withTranslation } from '../../i18n';
 
 const Text = styled.p`
     font-family: 'Roboto', sans-serif;
     font-weight: 500;
     font-size:1.25em;
 `;
-const WelcomeText = () => (
-  <Text className="text-center text-white pb-4">Seleccione el país que desea consultar:</Text>
+const WelcomeText = ({ t }) => (
+  <Text className="text-center text-white pb-4">{`${t('selectTheCountryYouWantToConsult')}:`}</Text>
 );
 
-export default WelcomeText;
+export default withTranslation('common')(WelcomeText);
