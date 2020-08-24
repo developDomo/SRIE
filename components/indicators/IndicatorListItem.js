abe07dd1d114e873e929c9d08b1d2f667b7f3c75
Francisco Campos <hey@francampos.me>
2020-08-24 08:55:16 -0600

    indicator list

--- components/indicators/IndicatorListItem.js
+++ components/indicators/IndicatorListItem.js
@@ -18,8 +18,9 @@ const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   align-items: center;
+  padding: 10px 0;
   width: 100%;
-  height: 130px;
+  height: auto;
   background-color: ${gray1};
 `;
 const Title = styled.h3`
@@ -27,13 +28,6 @@ const Title = styled.h3`
   font-weight: bold;
   font-size: 1.1em;
   color: ${txt};
-  line-height: 1.5em; /* Sets line height to 1.5 times text size */
-  height: 3em; /* Sets the div height to 2x line-height (3 times text size) */
-  width: 100%; /* Use whatever width you want */
-  white-space: normal; /* Wrap lines of text */
-  overflow: hidden; /* Hide text that goes beyond the boundaries of the div */
-  text-overflow: ellipsis; /* Ellipses (cross-browser) */
-  -o-text-overflow: ellipsis; /* Ellipses (cross-browser) */
 `;
 
 const Pec = styled.h4`
