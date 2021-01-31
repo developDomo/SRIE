2caa44cd871cdd8d53242d2151aa60693180c68b
Javier Camacho <javier.camacho@edify.cr>
2021-01-31 17:52:15 -0600

    CU-h8wdwt Add national data system link on dashboard page

--- services/Country.service.js
+++ services/Country.service.js
@@ -1,14 +1,14 @@
 const db = require('express-http-context').get('db');
 
 const options = {
-  fields: ['short_name'],
+  fields: ['short_name', 'data_system_name', 'data_system_link'],
   exprs: {
     code: 'lower(code)',
   },
 };
 
 export default {
-  findAll: async () => db.query('SELECT lower(code) as code, short_name FROM countries'),
+  findAll: async () => db.query('SELECT lower(code) as code, short_name, data_system_name, data_system_link FROM countries'),
   findByCode: async (code) => db.countries.findOne({ code: code.toUpperCase() }, options),
   findByShortName: async (shortName) => db.countries.findOne({ short_name: shortName.toLowerCase() }, options),
 };
